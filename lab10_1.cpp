4b6d6a86cc3cbd1f9018f52d3ab8e94907189290
Unknown <zidsod.6840@gmail.com>
2018-01-26 13:37:29 +0700

    ข้อที่ 1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,4 +1,15 @@
-int main() {
-        /* code */
-        return 0;
-}
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+    ofstream myfile ("example.txt");
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
+    }
