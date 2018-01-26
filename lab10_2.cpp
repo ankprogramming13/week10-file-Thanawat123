2f8cbd811260f901bec8acbede313fbcd0520b2d
Unknown <zidsod.6840@gmail.com>
2018-01-26 14:05:53 +0700

    ข้อที่ 2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,19 @@
-int main() {
-        /* code */
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  if (myfile.is_open())
+  {
+      while (getline (myfile,line))
+      {
+          cout << line << '\n';
+      }
+      myfile.close();
+  }
+        else cout << "unable to open file";
         return 0;
 }
